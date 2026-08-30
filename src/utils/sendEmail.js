9832f7857aaae98481ab90046e962b93cdbe974f
3ntar <abdoantar1006@gmail.com>
2026-08-30 22:42:27 +0300

    feat: switch email provider to resend http api

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,20 +1,30 @@
-const { Resend } = require('resend');
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
-    const { data, error } = await resend.emails.send({
-        from: 'Azez Store <onboarding@resend.dev>', // النطاق الافتراضي المجاني من Resend
+    // إجبار الاتصال على IPv4 ومنفذ 587 لتفادي حظر شبكات Render
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // false لاستخدام STARTTLS على منفذ 587
+        requireTLS: true,
+        family: 4, // إجبار الاتصال عبر IPv4 لمنع خطأ ENETUNREACH
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD,
+        },
+        connectionTimeout: 15000,
+        greetingTimeout: 15000,
+        socketTimeout: 20000,
+    });
+
+    const mailOptions = {
+        from: `"Azez Store" <${process.env.EMAIL_USER}>`,
         to: options.email,
         subject: options.subject,
         html: options.html || options.message,
-    });
+    };
 
-    if (error) {
-        throw new Error(error.message);
-    }
-
-    return data;
+    await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
